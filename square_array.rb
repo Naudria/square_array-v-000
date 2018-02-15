f95d329f2114003ea6d661f85ac6db8afad55648
Rima Jean <rima.jean@gmail.com>
2018-02-15 17:17:44 +0000

    automatically backed up by learn

--- square_array.rb
+++ square_array.rb
@@ -1,4 +1,10 @@
-def square_array(array)
-  array = [1, 2, 3]
-  
+def square_array(numbers)
+  new_numbers = []
+
+  numbers.each do |number|
+    new_numbers.push(number*number)
+  end
+
+  return new_numbers
+
 end
